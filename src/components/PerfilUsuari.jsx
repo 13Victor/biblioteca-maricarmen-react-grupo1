1aa7939813388d71ab903133efb71d157fecb278
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2025-04-28 20:43:51 +0200

    Fix centre nom

--- src/components/PerfilUsuari.jsx
+++ src/components/PerfilUsuari.jsx
@@ -38,13 +38,16 @@ export default function PerfilUsuari() {
   const handleSave = async () => {
     try {
       const formData = new FormData();
-      formData.append("payload", JSON.stringify({
-        id: usuari.id,
-        email: editedData.email,
-        first_name: editedData.first_name,
-        last_name: editedData.last_name,
-        telefon: editedData.telefon, 
-      }));
+      formData.append(
+        "payload",
+        JSON.stringify({
+          id: usuari.id,
+          email: editedData.email,
+          first_name: editedData.first_name,
+          last_name: editedData.last_name,
+          telefon: editedData.telefon,
+        })
+      );
 
       if (editedData.imatge) {
         formData.append("imatge", editedData.imatge);
@@ -73,24 +76,74 @@ export default function PerfilUsuari() {
     } catch (error) {
       alert("Error en la conexió amb el servidor.", error);
     }
-};
+  };
 
   if (errorProfile) return <div>{errorProfile}</div>;
   if (!usuari) return <></>;
 
   return (
     <div id="perfilUsuari-container">
-    <h2>{usuari.username}</h2>
-    <div id="perfilUsuari-info">
-      <div><strong>Nom: </strong>{isEditing ? <input type="text" name="first_name" value={editedData.first_name} onChange={handleInputChange} /> : usuari.first_name}</div>
-      <div><strong>Cognom: </strong>{isEditing ? <input type="text" name="last_name" value={editedData.last_name} onChange={handleInputChange} /> : usuari.last_name}</div>
-      <div><strong>Email: </strong>{isEditing ? <input type="email" name="email" value={editedData.email} onChange={handleInputChange} /> : usuari.email}</div>
-      <div><strong>Telèfon: </strong>{isEditing ? <input type="tel" name="telefon" maxLength="9" value={editedData.telefon || ''} onChange={handleInputChange} /> : usuari.telefon}</div>
-      <div><strong>Imatge: </strong>{isEditing ? <input type="file" name="imatge" accept="image/*" onChange={handleImageChange} /> : (usuari.imatge && <img src={`../..${usuari.imatge}`} alt="Imagen de perfil" width="100" />)}</div>
-      <div><strong>Centre: </strong>{usuari.centre}</div>
-      <div><strong>Grup: </strong>{usuari.grup}</div>
-      <div><strong>Rol: </strong>{usuari.is_staff && usuari.is_superuser ? "Administrador" : usuari.is_staff ? "Bibliotecari" : "Usuari"}</div>
-    </div>
+      <h2>{usuari.username}</h2>
+      <div id="perfilUsuari-info">
+        <div>
+          <strong>Nom: </strong>
+          {isEditing ? (
+            <input type="text" name="first_name" value={editedData.first_name} onChange={handleInputChange} />
+          ) : (
+            usuari.first_name
+          )}
+        </div>
+        <div>
+          <strong>Cognom: </strong>
+          {isEditing ? (
+            <input type="text" name="last_name" value={editedData.last_name} onChange={handleInputChange} />
+          ) : (
+            usuari.last_name
+          )}
+        </div>
+        <div>
+          <strong>Email: </strong>
+          {isEditing ? (
+            <input type="email" name="email" value={editedData.email} onChange={handleInputChange} />
+          ) : (
+            usuari.email
+          )}
+        </div>
+        <div>
+          <strong>Telèfon: </strong>
+          {isEditing ? (
+            <input
+              type="tel"
+              name="telefon"
+              maxLength="9"
+              value={editedData.telefon || ""}
+              onChange={handleInputChange}
+            />
+          ) : (
+            usuari.telefon
+          )}
+        </div>
+        <div>
+          <strong>Imatge: </strong>
+          {isEditing ? (
+            <input type="file" name="imatge" accept="image/*" onChange={handleImageChange} />
+          ) : (
+            usuari.imatge && <img src={`../..${usuari.imatge}`} alt="Imagen de perfil" width="100" />
+          )}
+        </div>
+        <div>
+          <strong>Centre: </strong>
+          {usuari.centre.nom}
+        </div>
+        <div>
+          <strong>Grup: </strong>
+          {usuari.grup}
+        </div>
+        <div>
+          <strong>Rol: </strong>
+          {usuari.is_staff && usuari.is_superuser ? "Administrador" : usuari.is_staff ? "Bibliotecari" : "Usuari"}
+        </div>
+      </div>
       <div id="perfilUsuari-buttons-container">
         {isEditing ? (
           <>
